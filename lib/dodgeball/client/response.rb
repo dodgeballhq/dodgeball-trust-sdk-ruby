4922de7b1a54f511d2b688eea4c916ebfc49da0b
Alex Paluzzi <alexpaluzzi@gmail.com>
2023-01-15 18:04:24 -0500

    checkpoint response helpers

--- lib/dodgeball/client/response.rb
+++ lib/dodgeball/client/response.rb
@@ -12,6 +12,46 @@ module Dodgeball
         @status = status
         @response_body = response_body
       end
+
+      def is_running
+        return false unless @response_body[:success]
+
+        case @response_body[:verification][:status]
+        when 'PENDING', 'BLOCKED'
+          true
+        else
+          false
+        end
+      end
+
+      def is_allowed
+        @response_body[:success] &&
+          @response_body[:verification][:status] == 'COMPLETE' &&
+          @response_body[:verification][:outcome] == 'APPROVED'
+      end
+
+      def is_denied
+        return false unless @response_body[:success]
+
+        @response_body[:verification][:outcome] == 'DENIED'
+      end
+
+      def is_undecided
+        @response_body[:success] &&
+          @response_body[:verification][:status] == 'COMPLETE' &&
+          @response_body[:verification][:outcome] == 'PENDING'
+      end
+
+      def has_error
+        !@response_body[:success] &&
+          (@response_body[:verification][:status] == 'FAILED' &&
+           @response_body[:verification][:outcome] == 'ERROR') ||
+          (@response_body[:errors] && !@response_body[:errors].empty?)
+      end
+
+      def is_timeout
+        !@response_body[:success] && @response_body[:isTimeout]
+      end
     end
   end
 end
